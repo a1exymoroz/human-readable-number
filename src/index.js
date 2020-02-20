cd72943a0d35d4df4474a03e933237a9da4005e1
Aleksei Morozov <aleksei.morozov@mera.com>
2020-02-20 15:52:52 +0300

    init: start Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+    const beforeTwenty = [
+        'zero', 'one', 'two', 'three', 'four',
+        'five', 'six', 'seven', 'eight', 'nine',
+        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+        'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+    const afterTwenty = [
+        '', '', 'twenty', 'thirty', 'forty',
+        'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+    ];
+
+    if (number < 20) {
+        return beforeTwenty[number]
+    } else {
+        const [ huns, tens, ones ] = String(number);
+        const strArray = [
+            (Number(huns) || 0) === 0 ? '' : beforeTwenty[huns] + ' hundred',
+            afterTwenty[tens],
+            (Number(ones) || 0) === 0 ? '' : beforeTwenty[tens]
+        ];
+
+        return strArray.join(' ');
+    }
 }
